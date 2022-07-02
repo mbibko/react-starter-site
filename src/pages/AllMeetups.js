13ef54ac0dea53561b0c3b7ea6ac99fc2a250c40
Maksym Bibko <mbibko@gmail.com>
2022-07-03 01:05:52 +0300

    added: load All meetups from server

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,31 +1,53 @@
+import { useEffect, useState } from "react";
 import { MeetupListGrid } from "../components/meetups/MeetupListGrid";
-
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+import { Link } from "react-router-dom";
 
 export function AllMeetups() {
+  const [meetups, setMeetups] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsError(false);
+    fetch("https://learn-react-6a98d-default-rtdb.firebaseio.com/meetups.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        setMeetups(
+          data && Object.keys(data).map((key) => ({ id: key, ...data[key] }))
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        setIsError(true);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError)
+    return <p>Error load Meetups. Please check internet connection</p>;
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupListGrid data={DUMMY_DATA} />
+
+      {meetups?.length > 0 ? (
+        <MeetupListGrid data={meetups} />
+      ) : (
+        <>
+          <p>No meetups here. Please add new one.</p>
+          <Link to={"/new-meetup"} className="btn btn-primary">
+            Add meetup
+          </Link>
+        </>
+      )}
     </section>
   );
 }
